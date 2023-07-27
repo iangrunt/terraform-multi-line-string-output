dfc075ceaba610b68409630a287813b72240d5fb
Ian <101607944+iangrunt@users.noreply.github.com>
2023-07-27 13:25:06 -0400

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output string {
+  value = <<EOT
+SCENE 2
+
+Another part of the island
+
+Enter CALIBAN, with a burden of wood. A noise of thunder heard
+
+  CALIBAN. All the infections that the sun sucks up
+    From bogs, fens, flats, on Prosper fall, and make him
+    By inch-meal a disease! His spirits hear me,
+    And yet I needs must curse. But they'll nor pinch,
+    Fright me with urchin-shows, pitch me i' th' mire,
+    Nor lead me, like a firebrand, in the dark
+    Out of my way, unless he bid 'em; but
+    For every trifle are they set upon me;
+    Sometime like apes that mow and chatter at me,
+    And after bite me; then like hedgehogs which
+    Lie tumbling in my barefoot way, and mount
+    Their pricks at my footfall; sometime am I
+    All wound with adders, who with cloven tongues
+    Do hiss me into madness.
+EOT
+}
